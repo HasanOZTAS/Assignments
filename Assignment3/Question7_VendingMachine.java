dc0b499cf05d463bf88104482da2ebcde83cae91
HasanOZTAS <80967660+HasanOZTAS@users.noreply.github.com>
2021-04-13 10:31:45 +0300

    Add files via upload

--- Assignment3/Question7_VendingMachine.java
+++ Assignment3/Question7_VendingMachine.java
@@ -6,13 +6,13 @@ public class Question7_VendingMachine {
 		
 		// 1 dollar == 100 cents
 		
-		int price=55;
+		int price=95;
 				
 		int change=100-price;
 		
 		int quarters=change/25;		
 		int dimes=(change%25)/10;
-		int nickles=(change%10)/5;
+		int nickles=((change%25)%10)/5;
 		
 			
 		System.out.println("Your change is " + quarters + " quarters, " + dimes + " dimes, " + nickles + " nickles");
